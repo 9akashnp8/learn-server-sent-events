846df47aed383f7719c5313888c2124cb09bc85a
Akash NP <akashnp1998@gmail.com>
2023-11-09 18:34:01 +0400

    feat: render sse events as streaming live chat response on ui
    - uses Azure's fetchEventSource instead of the EventSource API: the standard
    EventSource API just keeps sending requests for some reason, closing the
    connection on disconnection might solve this but continue using fetchEventSource
    given its additional features.
    - update the aiResponse and messageHistory state via callback, having
    the state update inside event listeners only take the state value of
    initial render. Through callback, we can ensure that latest state updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,37 @@
 
+import { useEffect, useState } from 'react';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+
+import type { ChatMessageType } from './types';
 import ChatInput from './components/ChatInput';
 import ChatMessagesWindow from './components/ChatMessagesWindow';
 
+type eventSourceCallbackHandler = (data: string, event: string) => void
+
+function eventSourceWrapper(cb: eventSourceCallbackHandler) {
+  fetchEventSource('http://localhost:8000/stream', {
+    onmessage(ev) {
+      cb(ev.data, ev.event)
+    },
+  })
+}
+
 function App() {
+  const [ aiResponse, setAiResponse ] = useState("");
+  const [ messageHistory, setMessageHistory] = useState<ChatMessageType[]>([])
+
+  useEffect(() => {
+    let msg = ''
+    eventSourceWrapper((data, eventType) => {
+      if (eventType == 'end') {
+        setMessageHistory([...messageHistory, {user: 'system', content: msg}])
+        setAiResponse("")
+      } else {
+        msg += data
+        setAiResponse(msg)
+      }
+    })
+  }, [messageHistory])
 
   return (
     <main>
